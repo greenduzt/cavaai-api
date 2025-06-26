7f2704f5f3e870ac48a489982e5be27a814489aa
greenduzt <chamara_sachintha@yahoo.com>
2025-06-26 23:53:41 +1000

    Update

--- server.py
+++ server.py
@@ -42,5 +42,8 @@ async def get_token():
     
     return token.to_jwt()
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
